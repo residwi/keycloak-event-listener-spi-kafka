cdacebbf1a66ea4e047d6d16cc04ab3944aff196
Resi Dwi <residwithawasa@gmail.com>
2021-04-14 11:38:31 +0700

    Add verifyEmail and deleteUser Event

--- src/main/java/com/github/residwi/plugin/keycloak/KafkaEventListenerProvider.java
+++ src/main/java/com/github/residwi/plugin/keycloak/KafkaEventListenerProvider.java
@@ -6,18 +6,21 @@ import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.events.admin.OperationType;
+import org.keycloak.events.admin.ResourceType;
 
 public class KafkaEventListenerProvider implements EventListenerProvider {
 
     @Override
     public void onEvent(Event event) {
         publishEvent("registerEvent", EventType.REGISTER, event);
-//        publishEvent("loginEvent", EventType.LOGIN, event);
+        publishEvent("loginEvent", EventType.LOGIN, event);
+        publishEvent("verifyEmailEvent", EventType.VERIFY_EMAIL, event);
     }
 
     @Override
     public void onEvent(AdminEvent adminEvent, boolean b) {
-        // default implementation ignored
+        publishAdminEvent("deleteUserAdminEvent", OperationType.DELETE, ResourceType.USER, adminEvent);
     }
 
     @Override
@@ -26,11 +29,21 @@ public class KafkaEventListenerProvider implements EventListenerProvider {
     }
 
     private void publishEvent(String topic, EventType eventType, Event event) {
-        ObjectMapper objectMapper = new ObjectMapper();
-
         if (event.getType().equals(eventType)) {
+            ObjectMapper objectMapper = new ObjectMapper();
             try {
                 KafkaMessageProducer.publishEvent(topic, objectMapper.writeValueAsString(event));
+            } catch (JsonProcessingException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    private void publishAdminEvent(String topic, OperationType operationType, ResourceType resourceType, AdminEvent adminEvent) {
+        if (adminEvent.getOperationType().equals(operationType) && adminEvent.getResourceType().equals(resourceType)) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                KafkaMessageProducer.publishEvent(topic, objectMapper.writeValueAsString(adminEvent));
             } catch (JsonProcessingException exception) {
                 exception.printStackTrace();
             }
